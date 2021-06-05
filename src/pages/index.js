edf46f217022b553af051192b50151399f705db8
vincenttse15 <vincenttse15@gmail.com>
2021-06-04 21:58:33 -0700

    changed colors and layout of website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import * as React from "react"
 import Navbar from "../components/Navbar";
 import * as styles from "../styles/index.module.scss";
-import Footer from "../components/Footer";
+//import Footer from "../components/Footer";
+import music from "../images/music.png";
+import band from "../images/band.png";
 
 const IndexPage = () => {
   return (
@@ -11,85 +13,23 @@ const IndexPage = () => {
         <div className={styles.home_container}>
           <div>
             <h1 className={styles.home_h1}>
-              Vincent Tse
+              HI, I'M <span className={styles.home_highlight}>VINCENT</span>.
             </h1>
           </div>
           <div>
             <h2 className={styles.home_h2}>
-              Software Engineer
+              I'm a <span className={styles.home_highlight}>Software Engineer</span> and a recent graduate from San Francisco State University.
             </h2>
-          </div>
-          <div>
-            <h3 className={styles.home_h3}>
-              San Jose
-            </h3>
           </div>
         </div>
-        <div className={styles.portfolio_container}>
-          <div className={styles.portfolio_heading}>
-            <h1 className={styles.portfolio_h1} id="projects">
-              Projects
-            </h1>
+        <div className={styles.doremeet_container}>
+          <div className={styles.image_container}>
+            <img src={music} alt="music" className={styles.music}></img>
+            <img src={band} alt="band"></img>
           </div>
-          <div className={styles.project_container}>
-            <h2 className={styles.project_title}>
-              DoReMeet
-            </h2>
-            <span className={styles.project_description}>
-              The first application I built using ReactJS with a team of six as front-end engineer.
-              This application was created to link artists by genre and meeting preference (virtual or non virtual).
-            </span>
-            <h3 className={styles.project_technology}>
-              Technology
-            </h3>
-            <div className={styles.technology_container}>
-              <span className={styles.technology}>JavaScript</span>
-              <span className={styles.technology}>React.js</span>
-              <span className={styles.technology}>Node.js</span>
-              <span className={styles.technology}>Express.js</span>
-            </div>
+          <div className={styles.description_container}>
+            <span className={styles.project_title}>DoReMeet</span>
           </div>
-          <div className={styles.project_container}>
-            <h2 className={styles.project_title}>
-              Marketplace 2.0
-            </h2>
-            <span className={styles.project_description}>
-              An e-commerce application I developed with a team of four as full-stack engineer.
-            </span>
-            <h3 className={styles.project_technology}>
-              Technology
-            </h3>
-            <div className={styles.technology_container}>
-              <span className={styles.technology}>JavaScript</span>
-              <span className={styles.technology}>TypeScript</span>
-              <span className={styles.technology}>MongoDB</span>
-              <span className={styles.technology}>React.js</span>
-              <span className={styles.technology}>Redux</span>
-              <span className={styles.technology}>Node.js</span>
-              <span className={styles.technology}>Express.js</span>
-            </div>
-          </div>
-          <div className={styles.project_container}>
-            <h2 className={styles.project_title}>
-              Database Project
-            </h2>
-            <span className={styles.project_description}>
-              Designed a command line application from the ground up that utilizes a database created
-              in MySQL purposed for a University.
-            </span>
-            <h3 className={styles.project_technology}>
-              Technology
-            </h3>
-            <div className={styles.technology_container}>
-              <span className={styles.technology}>Node.js</span>
-              <span className={styles.technology}>Express.js</span>
-              <span className={styles.technology}>MySQL</span>
-            </div>
-          </div>
-        </div>
-        <div className={styles.contact_container} id="contact">
-          <h1 className={styles.contact_h1}>Contact</h1>
-          <Footer />
         </div>
       </main>
     </body>
