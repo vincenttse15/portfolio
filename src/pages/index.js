a0ed18350b2e5579b3f0cf1b3eb0dd21c9694f4f
vincenttse15 <vincenttse15@gmail.com>
2021-06-07 19:15:06 -0700

    added database project and socials

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,51 @@
 import * as React from "react"
 import Navbar from "../components/Navbar";
 import * as styles from "../styles/index.module.scss";
-//import Footer from "../components/Footer";
 import music from "../images/music.png";
 import band from "../images/band.png";
-import ecommerce from "../images/ecommerce.png"
+import ecommerce from "../images/ecommerce.png";
+import database from "../images/database.png";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const IndexPage = () => {
+  library.add(fab, fas);
+
   return (
     <body id="top">
       <main>
         <Navbar />
         <div className={`${styles.home_container} ${styles.padding}`}>
-          <div>
-            <h1 className={styles.home_h1}>
-              HI, I'M <span className={styles.home_highlight}>VINCENT</span>.
-            </h1>
+          <div className={styles.home_text_container}>
+            <div>
+              <h1 className={styles.home_h1}>
+                HI, I'M <span className={styles.home_highlight}>VINCENT</span>.
+              </h1>
+            </div>
+            <div>
+              <h2 className={styles.home_h2}>
+                I'm a <span className={styles.home_highlight}>Software Engineer</span> and a recent graduate from San Francisco State University.
+              </h2>
+            </div>
           </div>
-          <div>
-            <h2 className={styles.home_h2}>
-              I'm a <span className={styles.home_highlight}>Software Engineer</span> and a recent graduate from San Francisco State University.
-            </h2>
+          <div className={styles.socials_container}>
+            <a className={styles.social_link} href="https://github.com/vincenttse15">
+              <FontAwesomeIcon icon={['fab', 'github-square']} className={styles.social_image} />
+            </a>
+            <a className={styles.social_link} href="https://www.linkedin.com/in/vincent-tse-0084a7210/">
+              <FontAwesomeIcon icon={['fab', 'linkedin']} className={styles.social_image} />
+            </a>
+            <a className={styles.social_link} href="mailto:vincenttse19@gmail.com">
+              <FontAwesomeIcon icon={['fas', 'envelope-square']} className={styles.social_image} />
+            </a>
           </div>
         </div>
         <div className={`${styles.doremeet_container} ${styles.padding}`}>
           <div className={styles.image_container}>
-            <img src={music} alt="music" className={styles.music}></img>
-            <img src={band} alt="band" className={styles.band}></img>
+            <img src={music} alt="music" className={styles.image}></img>
+            <img src={band} alt="band" className={styles.image}></img>
           </div>
           <div className={styles.description_container}>
             <span className={styles.project_title}>DoReMeet</span>
@@ -45,7 +64,7 @@ const IndexPage = () => {
         </div>
         <div className={`${styles.marketplace_container} ${styles.padding}`}>
           <div className={styles.image_container}>
-            <img src={ecommerce} alt="ecommerece"></img>
+            <img src={ecommerce} alt="ecommerece" className={styles.image}></img>
           </div>
           <div className={styles.description_container}>
             <span className={styles.project_title}>Marketplace 2.0</span>
@@ -59,6 +78,24 @@ const IndexPage = () => {
               <span>MongoDB</span>
               <span>Node.js</span>
               <span>Express.js</span>
+            </div>
+          </div>
+        </div>
+        <div className={`${styles.database_container} ${styles.padding}`}>
+          <div className={styles.image_container}>
+            <img src={database} alt="database" className={styles.image}></img>
+          </div>
+          <div className={styles.description_container}>
+            <span className={styles.project_title_bright}>Database Project</span>
+            <span className={styles.project_description_bright}>
+              Designed a command line application from the ground up that utilizes a database created
+              in MySQL purposed for a University.
+            </span>
+            <span className={styles.project_technology_title_bright}>Technology</span>
+            <div className={styles.project_technology}>
+              <span>Node.js</span>
+              <span>Express.js</span>
+              <span>MySQL</span>
             </div>
           </div>
         </div>
