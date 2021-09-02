7da812d455db04b0e7f3f5027dd62c8b855b6613
vincenttse15 <vincenttse15@gmail.com>
2021-09-01 18:25:20 -0700

    updated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,17 @@ module.exports = {
     },
     "gatsby-plugin-fontawesome-css",
     `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: 'src/images/logo2.svg',
+      },
+    },
   ],
 };
